7e4639e3a5063078223b31a5ed5f71d3f80ffd66
tanmay-Kumar-Swain <129646121+tanXnyx@users.noreply.github.com>
2023-10-17 22:29:54 +0530

    Add files via upload

---
+++ Q1_1068.java
@@ -0,0 +1,65 @@
+/*name- tanmay kumar swain
+ * reg 2241019632
+ */
+import java.util.*;
+import java.io.*;
+public class Q1_1068 {
+	public static BufferedReader br;
+	public static StringTokenizer st;
+	public static FastReader in = new FastReader();
+	public static long n = in.nextLong();
+	
+	public static void main(String[] args) throws IOException{
+		System.out.print(n + " ");
+		while(n != 1) {
+			if(n%2 == 1) {
+				n *= 3;
+				n++;
+			}
+			else {
+				n/=2;
+			}
+			System.out.print(n + " ");
+			
+			if(n == 0) {
+				break;
+			}
+		}
+	}
+	
+	static class FastReader{
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		String next() {
+			while(st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				}
+				catch(IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			}
+			catch(IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+}
---
+++ Q8_1092.java
@@ -0,0 +1,57 @@
+/*name- tanmay kumar swain
+ * reg 2241019632
+ */
+import java.util.*;
+
+import java.io.*;
+public class Q8_1092 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+        
+
+                int prod = (n * (n + 1)) / 2;
+                if (prod % 2 != 0)
+                    System.out.println("NO");
+                else {
+                    HashSet<Integer> set1 = new HashSet<>();
+                    HashSet<Integer> set2 = new HashSet<>();
+                    int i;
+                    int j = n;
+                    if (n % 2 == 0) {
+                        i = 1;
+                    } else {
+                        set1.add(1);
+                        set1.add(2);
+                        set2.add(3);
+                        i = 4;
+                    }
+                    while (i <= j) {
+                        set1.add(i);
+                        ++i;
+                        set2.add(i);
+                        ++i;
+                        set1.add(j);
+                        --j;
+                        set2.add(j);
+                        --j;
+                    }
+                    System.out.println("YES");
+                    System.out.println(set1.size());
+                    StringBuilder str1 = new StringBuilder();
+                    for (int x : set1)
+                        str1.append(x).append(" ");
+                    System.out.println(str1.toString());
+                    System.out.println(set2.size());
+                    StringBuilder str2 = new StringBuilder();
+                    for (int x : set2)
+                        str2.append(x).append(" ");
+                    System.out.println(str2.toString());
+                }
+           
+	}
+
+	
+}
